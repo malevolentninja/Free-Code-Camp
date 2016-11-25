669cc88fdeab1c674b8278a56cbee295d73d63b8
Olivia Boertje <malevolentninja@gmail.com>
2016-11-25 19:13:55 +0000

    Create javascript.js
    working progress...

---
+++ Tribute/javascript.js
@@ -0,0 +1,12 @@
+$ document.ready (function) {
+
+}
+
+
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height()-$(window).height()){
+        alert("We're at the bottom of the page!!");
+    }
+});
