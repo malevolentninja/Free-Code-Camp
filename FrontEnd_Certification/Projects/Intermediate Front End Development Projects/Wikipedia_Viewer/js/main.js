f957ae826e517e19c9ad1d0e98b4db602416e5d4
Olivia Boertje <21951936+malevolentninja@users.noreply.github.com>
2017-12-07 17:41:57 +0000

    Create main.js
    main jquery section

---
+++ FrontEnd_Certification/Projects/Intermediate Front End Development Projects/Wikipedia_Viewer/js/main.js
@@ -0,0 +1,66 @@
+function searchWiki() {
+  // Clear previous search results if any
+  $("#search-result").empty();
+
+  // Perform ajax call to wiki
+  $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php',
+    data: {
+      action: 'query',
+      list: 'search',
+      srsearch: `${$("#search-text").val()}`,
+      srlimit: '10',
+      format: 'json',
+    },
+    dataType: 'jsonp',
+    // SUCCESS
+    success: function(response) {
+      var result = response.query.search;
+      result.forEach(function(res) {
+
+        //  Building the div that will contain info in res
+        var $div = $("<div>", {
+          class: "wikiResult"
+        });
+
+        // Heading
+        var $h = $("<h1>");
+        $h.html(res.title);
+
+        //  Snippet
+        var $p = $("<p>");
+        $p.html(res.snippet);
+        $p.append("...");
+
+        //Link to wiki
+        var $p2 = $("<p>");
+        $p2.addClass("linkToWiki");
+        var $a = $("<a>", {
+          href: `https://en.wikipedia.org/wiki/${res.title.replace(" ","_")}`,
+          target: "_blank"
+        });
+        $a.text("Go to Wiki...")
+        $p2.append($a);
+
+        //  Append to result div
+        $div.append($h);
+        $div.append($p);
+        $div.append($p2);
+
+        //  Append to results div
+        $("#search-results").append($div);
+      });
+    }
+  });
+}
+
+function searchWikiTitle(title){
+  
+}
+
+$(document).ready(function() {
+  $("#search-button").on("click", function(e) {
+    e.preventDefault();
+    searchWiki();
+  });
+});
